1d181c660d032ae83161525991e7a73351483571
Edgar <edgar.ahmadeev@gmail.com>
2017-10-04 16:37:10 +0300

    Практика 04.10.2017

---
+++ Ex12A/src/ru/itis/Main.java
@@ -0,0 +1,18 @@
+package ru.itis;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(), n = 0;
+        double eps = sc.nextDouble(), s = 0, t = 1;
+        while (Math.abs(t) > eps) {
+            s += t;
+            n++;
+            t *= 1.0*x/n;
+        }
+        System.out.println(n + " " + s);
+    }
+}
