24c378bf59f5a1f0a6aa878b07e5a0888ba140f6
Edgar <edgar.ahmadeev@gmail.com>
2017-10-04 12:56:00 +0300

    Решения задания 10 обновлены

--- Ex10B/src/ru/itis/Main.java
+++ Ex10B/src/ru/itis/Main.java
@@ -12,19 +12,19 @@ public class Main {
         System.out.print("Введите x = ");
         int x = sc.nextInt();
         System.out.print("Введите n = ");
-        int n = sc.nextInt(), fakt = 1 ;
+        int n = sc.nextInt(), fakt = 1;
         //s2 - общая сумма; chisl - чистлитель члена; znam - знаменатель члена
         double s2 = x, chisl = x, znam = 1;
         //вводим цикл по k от 1 до n (считаем со 2-го члена)
-        for(int k = 1; k <= n; k++){
+        for (int k = 1; k <= n; k++) {
             //увеличиваем факториал (2k)!
             fakt *= 2 * k * (2 * k - 1);
             //задаём числитель текущего члена
-            chisl *= (-1.0)*x*x*x*x;
+            chisl *= (-1.0) * x * x * x * x;
             //задаём знаменатель текущего члена
-            znam = fakt*(4*k+1);
+            znam = fakt * (4 * k + 1);
             //прибавляем текущий член к сумме
-            s2 += chisl/znam;    //те же замечания
+            s2 += chisl / znam;    //те же замечания
         }
         System.out.println("Результат S2 = " + s2);
     }
