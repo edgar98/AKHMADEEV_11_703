a2eb3bd42b8aa2d9906a9f71a1915e3a18e4fd7e
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-10 20:58:50 +0300

    Update Main.java

--- Ex12A/src/ru/itis/Main.java
+++ Ex12A/src/ru/itis/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt(), n = 0;
+        int x = sc.nextInt(), n = 0; //всегда целое. Это не очень хорошо, это же ограниичивает задачу.
         double eps = sc.nextDouble(), s = 0, t = 1;
         while (Math.abs(t) > eps) {
             s += t;
