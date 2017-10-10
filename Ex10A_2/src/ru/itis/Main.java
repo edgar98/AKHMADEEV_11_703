68ebeb273335be6ad93b4eba5b1bf1a0f2559caf
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-10 20:51:09 +0300

    Update Main.java

--- Ex10A_2/src/ru/itis/Main.java
+++ Ex10A_2/src/ru/itis/Main.java
@@ -4,13 +4,13 @@ package ru.itis;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // вводим x,n - элементы формулы
         System.out.print("Введите x = ");
-        int x = sc.nextInt();
+        int x = sc.nextInt(); // непонятно, почему х целый?
         System.out.print("Введите n = ");
         int n = sc.nextInt();
         // s1 - общая сумма; x0 - член последовательности
@@ -18,7 +18,8 @@ public class Main {
         // вводим цикл по k от 1 до n (считаем со 2-го члена)
         for(int k = 1; k <= n; k++){
             // вычисляем текущий член последовательности
-            x0 *= (-1.0)*x*x*(2.0*(k-1.0)+1.0)*(1.0/k)*(1.0/(2.0*k + 1.0));
+            x0 *= (-1.0)*x*x*(2.0*(k-1.0)+1.0)*(1.0/k)*(1.0/(2.0*k + 1.0)); //Да, что-то в этом роде
+            // я бы записала короче: x0 *= (-1.0)*x*x*(2*(k-1)+1)/(k*(2*k + 1));
             // добавляем его к сумме
             s1 += x0;
         }
